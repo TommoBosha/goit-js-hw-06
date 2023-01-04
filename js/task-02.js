844bf9f9876a4406d69a5db2459918121821b24b
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-01-04 22:52:12 +0200

    remove comments

--- js/task-02.js
+++ js/task-02.js
@@ -6,17 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-// Напиши скрипт, который для каждого элемента массива ingredients:
 
-// Создаст отдельный элемент < li >.Обзательно используй метод document.createElement().
-// Добавит название ингредиента как его текстовое содержимое.
-// Добавит элементу класс item.
-// После чего вставит все < li > за одну операцию в список ul#ingredients.
 
 const ingredientsEl = document.querySelector("#ingredients");
 
-console.log(ingredientsEl);
-console.log(ingredients);
 
 const listRef = ingredients.map((ingredient) => {
   const elementRef = document.createElement("li");
--- js/task-09.js
+++ js/task-09.js
@@ -2,8 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// Напиши скрипт, который изменяет цвета фона элемента < body >
-//   через инлайн стиль при клике на button.change - color и выводит значение цвета в span.color.
 
 const changeColorEl = document.querySelector('.change-color');
 const colorEl = document.querySelector('.color');
